200cc960d5954c5291b620c7e6b7ca05bdff5814
sago007 <poul@poulsander.com>
2014-10-19 17:03:52 +0200

    Example files as I use them

---
+++ config-scgi-example.js
@@ -0,0 +1,15 @@
+{    
+"application" : {
+      // user specific data  
+      "connection_string" : "mysql:database=oastat",
+      "media_path" : "../media",
+      "static_media" : "../static"
+   },
+
+       "service" : {  
+        "api" : "scgi",  
+        "ip" : "127.0.0.1",  
+        "port" : 8081  
+       }  
+    }  
+
